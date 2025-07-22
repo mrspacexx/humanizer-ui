c7d0d9a6bb4ba86b2437ee2fef0cc16e1fba7e61
mrspacexx <efehansteam@gmail.com>
2025-07-23 00:08:36 +0300

    Açıklayıcı commit mesajı

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
     e.preventDefault();
     setAuthError("");
     try {
-      const res = await fetch("/login", {
+      const res = await fetch("http://localhost:8000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: loginEmail, password: loginPassword }),
@@ -72,7 +72,7 @@ export default function Home() {
       return;
     }
     try {
-      const res = await fetch("/signup", {
+      const res = await fetch("http://localhost:8000/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: signupEmail, password: signupPassword }),
